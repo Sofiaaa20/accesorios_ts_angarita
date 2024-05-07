fca783bbe42e16a87d43338cff5b9add84abe24d
Sofiaaa20 <126484803+Sofiaaa20@users.noreply.github.com>
2024-05-07 06:55:38 -0500

    Update

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,26 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import cookieParser from "cookie-parser";
-
 import dotenv from "dotenv";
 dotenv.config();
 
 import register from './routes/register';
 import auth from './routes/auth';
 import checkDatabaseConnection from "./config/dbHealtCheck";
-import profile  from "./routes/profile";
-
+import verifyToken from "./middleware/verify-token";
+// import perfilController from "./controllers/profile-controller";
 
 const app = express().use(bodyParser.json());
 
 app.use('/register', register);
 app.use('/auth', auth);
 app.use(cookieParser());
-app.use('/profile', profile);
 
 checkDatabaseConnection()
   .then(() => {
     const PORT: string | number = process.env.PORT || 8000;
     app.listen(PORT, () => {
-      console.log('Servidor corriendo en el puerto' ,PORT);
+      console.log('Servidor corriendo en el puerto', PORT);
     });
   })
   .catch((error) => {
@@ -31,4 +29,3 @@ checkDatabaseConnection()
       error
     );
   });
-
--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,38 +1,11 @@
-import jwt from 'jsonwebtoken';
-import UserRepository from '../repositories/UserRepository';
-const bcrypt = require("bcryptjs");
+// routes/auth.ts
 import { Request, Response } from "express";
-import { generateToken } from '../helpers/jsonWebToken';
+import authMiddleware  from "../middleware/authMiddleware";
+import { generateToken } from "../helpers/jsonWebToken";
+import Auth from "../Dto/authDto";
 
-
-let auth = async (req: Request, res: Response) => {
-      try {
-        const {email, password} = req.body;
-        const result:any = await UserRepository.login(req.body);
-        const rows = result[0];
-      
-        
-         if (rows.length > 0){
-           const isPasswordValid = await bcrypt.compare(password, rows[0].password);
-           console.log(isPasswordValid);
-           
-           if (!isPasswordValid){
-            return res.status(201).json({ 
-               status: 'incorrect username or password',password: rows[0].password
-
-             });
-           }
-         }
-         const token = generateToken(email);
-        return res.status(200).json({ 
-          status: 'Correct password',
-          token
-
-        });
-      } catch (error) {
-        console.log(error);
-      }
-
-}
+const auth = async (req: Request, res: Response) => {
+    authMiddleware(req, res)
+};
 
 export default auth;
